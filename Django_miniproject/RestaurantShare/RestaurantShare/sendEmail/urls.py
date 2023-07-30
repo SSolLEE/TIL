6ec0f603b7a6bc6e70daffa06cea41b9367d427b
Sol LEE <datasoling@gmail.com>
2023-07-30 15:21:39 +0900

    url&html setting

---
+++ Django_miniproject/RestaurantShare/RestaurantShare/sendEmail/urls.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('send/', views.sendEmail)
+]
---
+++ Django_miniproject/RestaurantShare/RestaurantShare/shareRes/urls.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('restaurantDetail/', views.restaurantDetail),
+    path('restaurantCreate/', views.restaurantCreate),
+    path('categoryCreate/', views.categoryCreate)
+]

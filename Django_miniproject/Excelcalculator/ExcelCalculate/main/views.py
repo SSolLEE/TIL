c81ee0e34ae944b23912105f3b2f68c01dfd4441
Sol LEE <datasoling@gmail.com>
2023-08-02 23:49:21 +0900

    Analysis using Pandas

--- Django_miniproject/Excelcalculator/ExcelCalculate/main/views.py
+++ Django_miniproject/Excelcalculator/ExcelCalculate/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import *
+from random import *
+from sendEmail.views import *
 
 # Create your views here.
 def index(request):
-    return render(request, "main/index.html")
+    if 'user_name' in request.session.keys():
+        return render(request, "main/index.html")
+    else:
+        return redirect("main_signin")
 
 def signup(request):
     return render(request, "main/signup.html")
@@ -19,16 +25,80 @@ def join(request):
 
     print("사용자 정보 저장 완료됨!")
 
-    return redirect("main_verifyCode")
+    # 인증 코드 1000~9000 랜덤으로 하나 생성
+    code = randint(1000, 9000)
+    print("인증 코드 생성---------", code)
+
+    response = redirect("main_verifyCode") # 응답을 객체로 저장
+    response.set_cookie('code', code) # 인증 코드
+    response.set_cookie('user_id', user.id)
+
+    print("응답 객체 완성 ----------", response)
+    # 이메일 발송 함수 호출
+    send_result = send(email, code)
+    if send_result:
+        print("Main > views.py > 이메일 발송 중 완료된 거 같음....")
+        return response
+    else:
+        return HttpResponse("이메일 발송 실패!")    
+
 
 def signin(request):
     return render(request, "main/signin.html")
 
+def login(request):
+    # 로그인된 user만 이용할 수 있도록 구현
+    # 로그인 user 판단을 위해 세션 사용(verify함수)
+    # 세션 처리 진행
+    loginEmail = request.POST['loginEmail']
+    loginPW = request.POST['loginPW']
+    user = User.objects.get(user_email = loginEmail)
+    if user.user_password ==loginPW:
+        request.session['user_name'] = user.user_name # user가 회원가입 시 입력한 정보
+        request.session['user_email'] = user.user_email # user가 회원가입 시 입력한 정보
+        return HttpResponse("test")
+    
+
 def verifyCode(request):
     return render(request, "main/verifyCode.html")
 
-def verify(request):  # 인증이 완료되면 메인 화면으로 보내줘
+def verify(request):  
+    # user가 입력한 code값을 받아야 함
+    user_code = request.POST['verifyCode']
+    # 쿠키에 저장된 코드를 불러옴(join함수 확인)
+    cookie_code = request.COOKIES.get('code')
+    print("코드 확인: ", user_code, cookie_code)
+
+    if user_code == cookie_code:
+        user=User.objects.get(id=request.COOKIES.get('user_id'))
+        user.user_validate = 1 # True 1, False 0
+        user.save()
+
+        print("DB에 user_validate 업데이트-------")
+
+        response = redirect('main_index')
+
+        # 저장되어 있는 쿠키를 삭제
+        response.delete_cookie('code')
+        response.delete_cookie('user_id')
+        # response.set_cookie('user', user)
+        # 사용자 정보를 세션에 저장
+        request.session['user_name'] = user.user_name   # 로그인화면 구현
+        request.session['user_email'] = user.user_email # 로그인화면 구현
+        
+        return response
+
     return redirect('main_index')
 
 def result(request):
-    return render(request, "main/result.html")
+    if 'user_name' in request.session.keys():
+        return render(request, "main/result.html")
+    else:
+        return redirect("main_signin")
+    # return render(request, "main/result.html")
+
+def logout(request):
+    del request.session['user_name']
+    del request.session['user_email']
+
+    return redirect('main_signin')
--- Django_miniproject/Excelcalculator/ExcelCalculate/sendEmail/views.py
+++ Django_miniproject/Excelcalculator/ExcelCalculate/sendEmail/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 def send(receiverEmail, verifyCode):
-    return HttpResponse("sendEmail, send function!")
+    try:
+        content = {'verifyCode':verifyCode}
+        msg_html = render_to_string('sendEmail/email_format.html', content)
+        msg = EmailMessage(subject="[**회사]인증 코드 발송 메일", body=msg_html, 
+                           from_email="datasoling@gmail.com", bcc=[receiverEmail])
+        msg.content_subtype = "html"
+        msg.send()
+
+        print("sendEmail > views.py > send함수 임무 완료-------")
+        return True
+    
+    except:
+        print("sendEmail > views.py > send함수 임무 실패.......")
+        return False
+    # return HttpResponse("sendEmail, send function!")
